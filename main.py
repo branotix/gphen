974859e6a5aa8a17488b9127422678eee715d44b
branotix <2304060@student.duet.ac.bd>
2026-01-31 21:36:21 +0600

    i have delete my app.txt file

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ def home():
 @app.route("/health")
 def health():
     return "ok", 200
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
